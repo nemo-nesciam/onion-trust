54cdee3a4cf27954742722bfacbcb0f1396eda88
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 23:42:08 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -31797,3 +31797,5 @@ import stem.control; with stem.control.Controller.from_port() as controller: con
 import stem.control
 print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
 import hashlib
+import socket
+import stem.control
