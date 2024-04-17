6c57b76c66e8247f2ce6bce19188b4de2cd8e60f
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 21:59:57 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -24105,3 +24105,5 @@ some_variable = 'example data'
 some_variable = 'example data'
 import stem.control
 import socks
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
