ae29a47f5cd3f7edfef7376e2fce7ad1d5f0ea3d
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 18:09:55 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -6847,3 +6847,5 @@ import socket; socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050)
 import requests
 import hashlib
 some_variable = 'example data'
+import requests
+import stem.control
