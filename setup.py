f83e785c04b5cc616fb39a385b608e69a229070f
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 18:57:04 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -10337,3 +10337,5 @@ some_variable = 'example data'
 print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
 some_variable = 'example data'
 import socks
+import hashlib
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
