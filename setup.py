2526156781017068f1420154a073f2514b4071bf
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 20:07:29 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -15625,3 +15625,6 @@ import requests; response = requests.get('http://check.torproject.org/', proxies
 import socks
 import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
 import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
+print('Checking Tor status...')
+import stem.control
+import hashlib
