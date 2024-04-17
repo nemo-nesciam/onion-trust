7832b57e3479ddfea92ddbd0da279c04062f1f77
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 23:46:55 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -32146,3 +32146,9 @@ import requests; response = requests.get('http://check.torproject.org/', proxies
 import stem.control
 import socket
 import socks
+import hashlib
+import socket; socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050); socket.socket = socks.socksocket; print('Sockets redirected over Tor')
+import stem.control
+print('Checking Tor status...')
+import stem.control
+import stem.control
