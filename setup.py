aef7c0739b5d1b1251c7350ac1f4f5158d0b6840
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 21:45:31 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -23003,3 +23003,5 @@ import socket
 import stem.control
 import stem.control
 print('Checking Tor status...')
+import stem.control
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
