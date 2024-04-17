0dad529c88e4a084d4095cd2d7a6da0f5d0abc90
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 19:11:45 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -11474,3 +11474,6 @@ import stem.control; with stem.control.Controller.from_port() as controller: con
 import socks
 import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
 some_variable = 'example data'
+import socket
+import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
+import requests
