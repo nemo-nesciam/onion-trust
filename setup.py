39e774033c730632dad24d7949248cbc0ed2f0bc
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 23:42:56 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -31848,3 +31848,5 @@ import socket
 some_variable = 'example data'
 import socket
 import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
+import hashlib
+import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
