91b3bd9583126720212e6b3bbcd47673ddf1bbb6
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 23:39:41 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -31613,3 +31613,5 @@ print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest
 import socket
 import stem.control
 import socks
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
