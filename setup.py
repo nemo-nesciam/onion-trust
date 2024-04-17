469ee60b9184097fd52c2fef6c08340815548f8c
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 21:32:05 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -22013,3 +22013,5 @@ import socket; socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050)
 print('Checking Tor status...')
 import requests
 print('Checking Tor status...')
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
+import socket
