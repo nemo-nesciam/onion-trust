7e50431b47e2816c8e3c88a805fe3773d299aa0f
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 19:30:06 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -12838,3 +12838,5 @@ print('Checking Tor status...')
 import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
 import stem.control
 import hashlib
+import stem.control
+some_variable = 'example data'
