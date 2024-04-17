4c5a43ff90033b667f19e705c3c8825d4f5112c0
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 20:40:14 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -18134,3 +18134,6 @@ import socket; socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050)
 print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
 import stem.control
 import stem.control
+import requests
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
+import requests
