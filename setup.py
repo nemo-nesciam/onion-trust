a02391f7d8f7104b49899405830c5b1322840964
Torry <RheaPomarico4467@hotmail.com>
2024-04-17 17:23:00 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -3306,3 +3306,5 @@ import stem.control; with stem.control.Controller.from_port() as controller: con
 import socket
 import socks
 some_variable = 'example data'
+import stem.control
+import requests
