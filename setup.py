4591e0970ad1eb1bbb850be6d900d7b4e6239e5b
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 00:51:29 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -37008,3 +37008,6 @@ import stem.control; with stem.control.Controller.from_port() as controller: con
 import stem.control
 import stem.control
 import stem.control; with stem.control.Controller.from_port() as controller: controller.authenticate(); print('Tor is running' if controller.is_alive() else 'Tor is not running')
+import requests
+import socket
+print('Checking Tor status...')
