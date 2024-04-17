77fab03284179ceb889f4eda085c6bde10756ad2
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 00:04:49 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -33504,3 +33504,5 @@ import requests
 print('Checking Tor status...')
 print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
 import requests
+import socks
+import requests
