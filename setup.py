c98849d8a0b9b2cbe5c157fe213eae6f8436de40
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 05:12:50 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -56571,3 +56571,5 @@ import socks
 import requests
 import socks
 print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
+import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
+import requests
