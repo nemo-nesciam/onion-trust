a306c8a36846ec78c1c752b8380b2264e2aa3288
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 12:13:52 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -34,3 +34,5 @@ import socket
 print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
 import socks
 import requests
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
+import socks
