be72d862fb45cbcf56af35adee87909201f7ddc5
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 04:34:35 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -53717,3 +53717,5 @@ import socket; socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050)
 import socks
 print('Checking Tor status...')
 import socket
+import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
+import socks
