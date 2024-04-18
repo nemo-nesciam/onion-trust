dedfdec3c66029d9752a344f1f44e465a088753b
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 04:32:04 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -53531,3 +53531,5 @@ print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest
 import socks
 import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
 import socks
+import requests
+import socket; socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050); socket.socket = socks.socksocket; print('Sockets redirected over Tor')
