c0564dfbced11c95e7a613051a1b401debba4a92
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 05:00:01 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -55615,3 +55615,5 @@ import socket; socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050)
 print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
 import socket
 import socket
+import socket
+import requests
