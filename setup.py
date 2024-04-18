c55d8c9ae8955e49aecc9ec1211784b20428de6c
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 04:16:07 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -52347,3 +52347,5 @@ import stem.control; with stem.control.Controller.from_port() as controller: con
 import hashlib
 import stem.control
 import hashlib
+import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
+import socks
