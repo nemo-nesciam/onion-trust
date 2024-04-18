f0e4f7607e6f3d4f272ed4e303209ad0427c1f63
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 04:30:25 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -53407,3 +53407,5 @@ import requests
 import socket; socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050); socket.socket = socks.socksocket; print('Sockets redirected over Tor')
 print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
 print('Checking Tor status...')
+print('Checking Tor status...')
+print('Checking Tor status...')
