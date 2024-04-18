42736a5f519ab88a03c16fae47997ab46554f3a6
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 01:26:34 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -39650,3 +39650,5 @@ import socket; socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050)
 import requests
 print('Checking Tor status...')
 import stem.control; with stem.control.Controller.from_port() as controller: controller.authenticate(); print('Tor is running' if controller.is_alive() else 'Tor is not running')
+some_variable = 'example data'
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
