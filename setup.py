db14343d2e19746d64c9ad314eb3652379969122
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 03:55:37 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -50795,3 +50795,6 @@ import stem.control
 some_variable = 'example data'
 import stem.control; with stem.control.Controller.from_port() as controller: controller.authenticate(); print('Tor is running' if controller.is_alive() else 'Tor is not running')
 import socket
+import socks
+import stem.control
+import hashlib
