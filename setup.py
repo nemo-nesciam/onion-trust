e1423b6c552c2635375ced9d81a585c1570682a6
Torry <RheaPomarico4467@hotmail.com>
2024-04-18 05:20:10 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -57106,3 +57106,6 @@ import hashlib
 some_variable = 'example data'
 import stem.control; with stem.control.Controller.from_port() as controller: controller.authenticate(); print('Tor is running' if controller.is_alive() else 'Tor is not running')
 some_variable = 'example data'
+import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
+import socket
+import socks
