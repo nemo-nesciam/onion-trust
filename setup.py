f292b20cf6b8c436bc4c82955d44476cf96b6fd5
Torry <RheaPomarico4467@hotmail.com>
2024-04-19 13:54:15 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
 #config
+some_variable = 'example data'
 import requests
-print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
-print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
