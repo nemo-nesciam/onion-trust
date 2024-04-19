44801c69639814ed8a7fa2b8d7c4dfa7971e283f
Torry <RheaPomarico4467@hotmail.com>
2024-04-19 15:20:30 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
 #config
-import socks
 print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
-import hashlib
+print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
