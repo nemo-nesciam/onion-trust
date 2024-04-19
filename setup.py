92e47886e013b73da2adc48312dd4c1f2b16319f
Torry <RheaPomarico4467@hotmail.com>
2024-04-19 15:28:43 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
 #config
-import requests
-import requests
-import socks
+import requests; response = requests.get('http://check.torproject.org/', proxies={'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}); print(response.text)
+import socket
