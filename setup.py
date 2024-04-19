f6baf51a7ae5431a68fd906b3eb40486c8494421
Torry <RheaPomarico4467@hotmail.com>
2024-04-19 16:09:19 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #config
 import hashlib
-print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
-print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
+import requests
+import requests
