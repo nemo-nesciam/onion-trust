59cb85d624829245e9a401c7f50eea122639fcf9
Torry <RheaPomarico4467@hotmail.com>
2024-04-19 14:21:14 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
 #config
 import stem.control
-print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
-print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
+import requests
