4f1a94bc76b5066667b904d9b99e01365755b970
Torry <RheaPomarico4467@hotmail.com>
2024-04-20 16:55:37 +0100

    Config update with Python script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #config
-some_variable = 'example data'
-import hashlib
-print(f'Hash of the variable:', hashlib.sha256(some_variable.encode()).hexdigest())
+import requests
+import socks
+import stem.control
